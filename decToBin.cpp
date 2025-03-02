d79e47335b18e0fc7530ca37b242a11619a1cf04
Suman Maity <someone.cpp@proton.me>
2025-03-02 20:23:05 +0530

    Update decToBin.cpp

--- decToBin.cpp
+++ decToBin.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-
 using namespace std;
  
-int binToDec(int binNum){
+int decToBin(int decNum){
   int ans=0, pow=1;
-  while(binNum>0){
-    int rem=binNum%10;
-    ans+=rem*pow;
-    pow=pow*2;
-    binNum/=10;
+  while(decNum>0){
+    int rem = decNum % 2;
+    decNum = decNum / 2;
+    ans += (rem * pow);
+    pow = pow * 10;
   }
   return ans;
 }
 int main() {
-     cout<<"The decmal number is "<<binToDec(1100011);
+  // int decNum = 50;
+  // cout<<"The binary number is "<<decToBin(50);
+
+  for(int i=1;i<=10;i++){
+    cout<<"The binary number of "<< i << " is "<<decToBin(i)<<endl;
+  }
   return 0;
 }
